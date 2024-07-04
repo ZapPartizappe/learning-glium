6063723c0aaebe69532067466c0d9fda0cfefb51
ZapPartizappe <62827040+ZapPartizappe@users.noreply.github.com>
2024-07-05 00:15:34 +0300

    Update main.rs

--- glium-book/src/main.rs
+++ glium-book/src/main.rs
@@ -15,20 +15,21 @@ use glium::{
 #[derive(Clone, Copy)]
 struct Vertex {
 	position: [f32; 2],
+	color: [f32; 3],
 }
 
 fn main()
 {
-	implement_vertex!(Vertex, position);
+	implement_vertex!(Vertex, position, color);
 
 	let event_loop = EventLoopBuilder::new().build().expect("Event loop building...");
 
 	let (window, display) = SimpleWindowBuilder::new().build(&event_loop);
 
 	let shape = vec![
-		Vertex { position: [-0.5, -0.5] },
-		Vertex { position: [ 0.0,  0.5] },
-		Vertex { position: [ 0.5, -0.25] }
+		Vertex { position: [-0.5, -0.5], color: [1.0, 0.0, 0.0] },
+		Vertex { position: [ 0.0,  0.5], color: [0.0, 1.0, 0.0] },
+		Vertex { position: [ 0.5, -0.25], color: [0.0, 0.0, 1.0] }
 	];
 
 	let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
@@ -38,23 +39,27 @@ fn main()
 		#version 140
 
 		in vec2 position;
+		in vec3 color;
+		out vec3 vert_color;
 
 		uniform mat4 matrix;
 
 		void main()
 		{
 			gl_Position = matrix * vec4(position, 0.0, 1.0);
+			vert_color = color;
 		}
 	"#;
 
 	let frag_shader_src = r#"
 		#version 140
 
+		in vec3 vert_color;
 		out vec4 color;
 
 		void main()
 		{
-			color = vec4(1.0, 0.0, 0.0, 1.0);
+			color = vec4(vert_color, 1.0);
 		}
 	"#;
 
@@ -78,8 +83,7 @@ fn main()
 				WindowEvent::RedrawRequested => {
 					t += 0.02;
 
-					let t_cos = t.cos();
-					let t_sin = t.sin();
+					let (t_sin, t_cos) = t.sin_cos();
 
 					let uniforms = uniform! {
 						matrix: [
@@ -92,7 +96,7 @@ fn main()
 
 					let mut target = display.draw();
 
-					target.clear_color(0.0, 0.0, 1.0, 1.0);
+					target.clear_color(0.0, 0.0, 0.0, 1.0);
 					target.draw(
 						&vertex_buffer, &indices, &program, &uniforms,
 						&Default::default()
